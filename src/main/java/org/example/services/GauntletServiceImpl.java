e06e6bbf3f1160f834ca30f0ff444e208484870a
adrianhh1426 <adrianhh1426@gmail.com>
2026-02-22 17:52:08 -0600

    instance singleton class

--- src/main/java/org/example/services/GauntletServiceImpl.java
+++ src/main/java/org/example/services/GauntletServiceImpl.java
@@ -3,11 +3,12 @@ package org.example.services;
 import lombok.extern.java.Log;
 import org.example.models.Stone;
 import org.example.models.TimeStone;
+import org.example.singletons.MindStoneSingleton;
 
 @Log
 public class GauntletServiceImpl implements IGauntletService{
 
-    private Stone timeStone = new TimeStone();
+    private final Stone timeStone = MindStoneSingleton.getInstance();
 
     @Override
     public void useGauntlet(String stoneName) {
